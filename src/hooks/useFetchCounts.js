3b6fbedf2cc48741017e0704703132b78f2c8ea1
TheSumSt <2240765672@qq.com>
2025-03-19 05:36:48 +0800

    feat: use real api handle method fix: fix the data use method bug

--- src/hooks/useFetchCounts.js
+++ src/hooks/useFetchCounts.js
@@ -7,15 +7,28 @@ export function useFetchCounts() {
 
   // TODO: 问题数量、回答数量请求接口
   const fetchQuestionCount = async () => {
-    // 假设有接口可以获取问题的数量
-    // let questionRes = await request.get('/api/admin/question/count');
-    // questionCount.value = questionRes.count;
+    try {
+      let countRes = await request.get(`/api/public/questionNum`);
+      // test
+      // console.log('@',countRes.count);
+      //   if (countRes.code === 200) {
+      //     questionCount.value = countRes.count;
+      //   } else {
+      //     console.error(countRes.message);
+      //   }
+      questionCount.value = countRes.count;
+    } catch (error) {
+      console.error('Error fetching question count:', error);
+    }
   };
 
   const fetchAnswerCount = async () => {
-    // 假设有接口可以获取回答的数量
-    // let answerRes = await request.get('/api/admin/answer/count');
-    // answerCount.value = answerRes.count;
+    try {
+      let countRes = await request.get(`/api/public/answerNum`);
+      answerCount.value = countRes.count;
+    } catch (error) {
+      console.error('Error fetching answer count:', error);
+    }  
   };
 
   return {
